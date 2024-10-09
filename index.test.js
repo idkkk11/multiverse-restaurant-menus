2d64df6bcccb5d6b424df282896cde71e90c8ca1
Michael Widjaya <michaeladhipramanaw@gmail.com>
2024-10-09 16:09:57 +0100

    db, models, and tests

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const {sequelize} = require('./db')
-const {Restaurant, Menu} = require('./models/index')
+const { sequelize } = require('./db')
+const { Restaurant, Menu } = require('./models/index')
 const {
     seedRestaurant,
     seedMenu,
-  } = require('./seedData');
+} = require('./seedData');
 
 describe('Restaurant and Menu Models', () => {
     /**
@@ -17,27 +17,64 @@ describe('Restaurant and Menu Models', () => {
     });
 
     test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await Restaurant.bulkCreate(seedRestaurant)
+        expect(await Restaurant.findByPk(1)).toEqual(
+            expect.objectContaining({
+                name: 'AppleBees',
+                location: 'Texas',
+                cuisine: 'FastFood'
+
+            })
+        )
     });
 
     test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await Menu.bulkCreate(seedMenu)
+        expect(await Menu.findByPk(1)).toEqual(
+            expect.objectContaining({
+                title: 'Breakfast'
+            },)
+        )
     });
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+    test('can update Restaurants', async () => {
+        await Restaurant.bulkCreate(seedRestaurant)
+        await Restaurant.update({
+            cuisine: "Faster Food"
+        }, { where: { name: "AppleBees" } })
+        expect(await Restaurant.findByPk(1)).toEqual(
+            expect.objectContaining({
+                name: 'AppleBees',
+                location: 'Texas',
+                cuisine: 'Faster Food'
+            },)
+        )
     });
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+    test('can update Menus', async () => {
+        await Menu.bulkCreate(seedMenu)
+        await Menu.update({
+            title: "Brunch"
+        }, { where: { title: "Breakfast" } })
+        expect(await Menu.findByPk(1)).toEqual(
+            expect.objectContaining({
+                title: "Brunch"
+            },)
+        )
     });
 
     test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await Restaurant.bulkCreate(seedRestaurant)
+        await Restaurant.destroy({where: {id: 1}})
+        expect(await Restaurant.findByPk(1)).toEqual(
+            null
+        )
+    });
+    test('can delete Menus', async () => {
+        await Menu.bulkCreate(seedMenu)
+        await Menu.destroy({where: {id: 1}})
+        expect(await Menu.findByPk(1)).toEqual(
+            null
+        )
     });
 })
--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,16 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes, Model} = require('sequelize');
 
-// TODO - create a Menu model
+class Menu extends Model {}
+
+Menu.init(
+    {
+        title: DataTypes.STRING
+    },
+    {
+        sequelize: sequelize,
+        modelName: "Menu"
+    }
+)
 
 module.exports = {Menu};
